11357a14d11ae20e121fd1ac1764df23d9745afa
russ044 <russ044@yandex.ru>
2022-09-06 01:45:36 +0400

    1.0

--- README.md
+++ README.md
@@ -1,27 +1,32 @@
+# API для Yatube.
+API для проекта YaTube, основанный на Django-Rest-Framework
+
+Документация к API: http://127.0.0.1:8000/redoc/
+
 ### Как запустить проект:
 
 Клонировать репозиторий и перейти в него в командной строке:
 
 ```
-git clone https://github.com/yandex-praktikum/kittygram2plus.git
+git clone https://github.com/russ044/api_final_yatube.git
 ```
 
 ```
-cd kittygram2plus
+cd api_final_yatube
 ```
 
 Cоздать и активировать виртуальное окружение:
 
 ```
-python3 -m venv env
+python -m venv env
 ```
 
 ```
-source env/bin/activate
+source env/Scripts/activate
 ```
 
 ```
-python3 -m pip install --upgrade pip
+python -m pip install --upgrade pip
 ```
 
 Установить зависимости из файла requirements.txt:
@@ -33,11 +38,11 @@ pip install -r requirements.txt
 Выполнить миграции:
 
 ```
-python3 manage.py migrate
+python manage.py migrate
 ```
 
 Запустить проект:
 
 ```
-python3 manage.py runserver
+python manage.py runserver
 ```
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,8 +3,8 @@ from rest_framework import mixins, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.filters import SearchFilter
 
-from posts.models import Group, Post
 from .permissions import IsAuthorOrReadOnly
+from posts.models import Group, Post
 from .serializers import (
     CommentSerializer,
     FollowSerializer,
@@ -16,13 +16,12 @@ from .serializers import (
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthorOrReadOnly]
+    permission_classes = (IsAuthorOrReadOnly,)
     pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
@@ -31,10 +30,7 @@ class PostViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly,
-        IsAuthorOrReadOnly,
-    ]
+    permission_classes = (IsAuthorOrReadOnly,)
 
     def get_post(self):
         return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
